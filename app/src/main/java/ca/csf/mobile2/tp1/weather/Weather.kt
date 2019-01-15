c38ebd02ce6468f4a1cdb96e2b1b8501333611b1
JonathanRN <minijona7@gmail.com>
2019-01-15 13:43:47 -0500

    init

---
+++ app/src/main/java/ca/csf/mobile2/tp1/weather/Weather.kt
@@ -0,0 +1,3 @@
+package ca.csf.mobile2.tp1.weather
+
+class Weather(val type : String = "SUNNY", val temperatureInCelcius : Int = 12, val city : String = "QC")
--- app/src/main/java/ca/csf/mobile2/tp1/weather/WeatherActivity.kt
+++ app/src/main/java/ca/csf/mobile2/tp1/weather/WeatherActivity.kt
@@ -2,13 +2,40 @@ package ca.csf.mobile2.tp1.weather
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.TextView
 import ca.csf.mobile2.tp1.R
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.beust.klaxon.Klaxon
 
 class WeatherActivity : AppCompatActivity() {
+
+    companion object {
+        const val URL = "https://m2t1.csfpwmjv.tk/api/v1/weather"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_weather)
+
+        val temperatureTextView = findViewById<TextView>(R.id.temperatureTextView)
+        temperatureTextView.visibility = View.VISIBLE
+
+        var weather : String? = null
+
+        val queue = Volley.newRequestQueue(this)
+        val stringRequest = StringRequest(Request.Method.GET, URL,
+            Response.Listener<String> { response ->
+                weather = response!!
+            },
+            Response.ErrorListener { /*Didnt work*/ })
+
+        queue.add(stringRequest)
+
+        temperatureTextView.text = weather
     }
 
 }
