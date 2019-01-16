197d7f74816b2bfc95fef5cf34dae6ec452b8868
JonathanRN <minijona7@gmail.com>
2019-01-16 08:03:42 -0500

    kinda working

--- app/src/main/java/ca/csf/mobile2/tp1/weather/Weather.kt
+++ app/src/main/java/ca/csf/mobile2/tp1/weather/Weather.kt
@@ -1,6 +1,3 @@
 package ca.csf.mobile2.tp1.weather
 
-class Weather(var type : String = "SUNNY", var temperatureInCelsius : Int = 12, var city : String = "QC")
-{
-
-}
+class Weather(val type : String, val temperatureInCelsius : Int, val city : String)
--- app/src/main/java/ca/csf/mobile2/tp1/weather/WeatherActivity.kt
+++ app/src/main/java/ca/csf/mobile2/tp1/weather/WeatherActivity.kt
@@ -5,36 +5,49 @@ import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.TextView
 import ca.csf.mobile2.tp1.R
-import com.android.volley.Request
-import com.android.volley.Response
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
 import com.beust.klaxon.Klaxon
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+import java.net.URL
 
 class WeatherActivity : AppCompatActivity() {
 
     companion object {
-        const val URL = "https://m2t1.csfpwmjv.tk/api/v1/weather"
+        const val WEATHER_URL = "https://m2t1.csfpwmjv.tk/api/v1/weather"
     }
+
+    var weather : Weather? = null
+    var temperatureTextView : TextView? = null
+    var cityTextView : TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_weather)
 
-        val temperatureTextView = findViewById<TextView>(R.id.temperatureTextView)
-        temperatureTextView.visibility = View.VISIBLE
+        temperatureTextView = findViewById(R.id.temperatureTextView)
+        cityTextView = findViewById(R.id.cityTextView)
 
-        var weather : Weather? = null
+        sendRequest()
 
-        val queue = Volley.newRequestQueue(this)
-        val stringRequest = StringRequest(Request.Method.GET, URL,
-            Response.Listener<String> { response ->
-                weather = Klaxon().parse<Weather>(response)
-                temperatureTextView.text = weather?.temperatureInCelsius.toString()
-            },
-            Response.ErrorListener { /*Didnt work*/ })
-
-        queue.add(stringRequest)
+        while(temperatureTextView?.visibility == View.INVISIBLE) {
+            println("waiting")
+        }
     }
 
+
+    fun sendRequest() {
+        doAsync {
+            val response = URL(WEATHER_URL).readText()
+            uiThread {
+                if (response.isNotBlank()) {
+                    temperatureTextView?.visibility = View.VISIBLE
+                    cityTextView?.visibility = View.VISIBLE
+
+                    weather = Klaxon().parse<Weather>(response)!!
+                    temperatureTextView?.text = weather?.temperatureInCelsius.toString()
+                    cityTextView?.text = weather?.city
+                }
+            }
+        }
+    }
 }
